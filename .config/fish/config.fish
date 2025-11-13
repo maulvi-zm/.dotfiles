93b02172e7df39a85c22991965410c2eb9c40cd7
maulvizm <13522122@std.stei.itb.ac.id>
2025-11-13 10:20:24 +0700

    feat: update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,21 +1,23 @@
 eval $(/opt/homebrew/bin/brew shellenv)
 
+# Aliases
+alias tcframe $TCFRAME_HOME/scripts/tcframe
+alias cd 'zoxide query'
+alias ls 'eza --icons=always'
+
 set -gx PATH $HOME/.cargo/bin $PATH
 set -gx PATH $HOME/.bun/bin $PATH
 set -gx PATH $HOME/development/flutter/bin $PATH
 set -gx PATH $HOME/.gem/bin $PATH
 set -gx CHROME_EXECUTABLE "/Applications/Arc.app/Contents/MacOS/Arc" # CHROME_EXECUTABLE for Flutter
+
 #if status is-interactive
 #    # Commands to run in interactive sessions can go here
 #end
 
 set -gx EDITOR nvim
-
 set -gx TCFRAME_HOME ~/tcframe
 
-# Aliases
-alias tcframe $TCFRAME_HOME/scripts/tcframe
-alias z 'zoxide query'
 
 starship init fish | source
 kubectl completion fish | source

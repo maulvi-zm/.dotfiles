0a984050f115b3360b4045f306888a3bc5ae2613
Maulvi Ziadinda Maulana <maulvi.maulana@MacBook-Pro-Maulvi.local>
2026-02-02 16:59:55 +0700

    chore: delete fish completion
    Completions functionality is now provided by carapace cli

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,14 +11,12 @@ set -gx PATH $HOME/development/flutter/bin $PATH
 set -gx PATH $HOME/.gem/bin $PATH
 set -gx CHROME_EXECUTABLE "/Applications/Arc.app/Contents/MacOS/Arc" # CHROME_EXECUTABLE for Flutter
 set -gx EDITOR nvim
-set -gx TCFRAME_HOME ~/tcframe
 
 # User env
 set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense' # optional
 
 carapace _carapace | source
 starship init fish | source
-kubectl completion fish | source
 zoxide init fish | source
 fnm env --use-on-cd | source
 fish_add_path $HOME/.local/bin
